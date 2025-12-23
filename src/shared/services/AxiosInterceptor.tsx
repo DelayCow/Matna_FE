ef34453be15cb0591fac274244d6dde75a49b1dc
wldusdn <wldusdngg@naver.com>
2025-12-23 14:52:57 +0900

    feat: Added Common api

---
+++ src/shared/services/AxiosInterceptor.tsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const AxiosInterceptor = axios.create({
+    baseURL: 'http://127.0.0.1:5432',
+    headers: {
+        'Content-Type' : 'application/json',
+    },
+})
+
+AxiosInterceptor.interceptors.request.use((config) => {
+    const token = sessionStorage.getItem('au');
+    if (token){
+        config.headers.Authorization = token;
+    }
+    return config;
+})
+
+AxiosInterceptor.interceptors.response.use(
+    (response) => {
+    const newToken = response.headers['authorization'];
+    
+    if (newToken && newToken.startsWith('Bearer ')) {
+            sessionStorage.setItem('au', newToken);
+        }    
+    return response;
+    },(error) => {
+        if (error.response?.status === 401) {
+            sessionStorage.removeItem('au');
+            if (!window.location.pathname.includes('/login')) {
+                window.location.href = '/login';
+            }
+        }
+        return Promise.reject(error);
+    })
+
+export default AxiosInterceptor;
