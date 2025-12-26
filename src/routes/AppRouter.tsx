2a6c08d36d01f1af3d7baf6116f7bfddbc2aac1c
wldusdn <wldusdngg@naver.com>
2025-12-26 11:54:45 +0900

    feat: Added ReviewList Page and api

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,6 +6,7 @@ import MobileLayout from "../shared/components/layout/MobileLayout";
 import RecipeHome from "../pages/RecipeHome";
 import GroupBuyHome from "../pages/GroupBuyHome";
 import MyPage from "../pages/MyPage";
+import ReviewList from "@/pages/ReviewList";
 
 const MemberRoute = function(){
     const token = sessionStorage.getItem('au');
@@ -23,6 +24,7 @@ export default function AppRouter(){
                             <Route path="/recipe" element={<RecipeHome />}/>
                             <Route path="/groupBuy" element={<GroupBuyHome />}/>
                             <Route path="/mypage" element={<MyPage />}/>
+                            <Route path="/review/recipe/:recipeNo" element={<ReviewList />}/>
                         </Route>
                     </Route>
                     
